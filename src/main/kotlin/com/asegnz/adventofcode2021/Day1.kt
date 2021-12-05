baaf06613ae103d1b86053009a84afc09b35c75f
Alberto Segovia Sanz <asegnz@gmail.com>
2021-12-05 11:30:59 +0100

    Adventofcode day 1

---
+++ src/main/kotlin/com/asegnz/adventofcode2021/Day1.kt
@@ -0,0 +1,41 @@
+package com.asegnz.adventofcode2021
+
+import java.io.File
+
+internal class Day1 {
+    companion object {
+        fun calculaMayores(filename: String): Int {
+            var contador = 0
+            var actual = Int.MAX_VALUE
+            File(filename).forEachLine {
+                val number = it.toInt()
+                if (number > actual) {
+                    contador++
+                    println(number)
+                }
+                actual = number
+            }
+            return contador
+        }
+
+        fun calculaVentana3(from: String): Int {
+            var valorVentanaAnterior = Int.MAX_VALUE
+            var contador = 0
+            var list = mutableListOf<Int>()
+
+            File(from).forEachLine {
+                val number = list.add(it.toInt())
+            }
+            var i = 0
+            while (i < list.size - 2) {
+                var ventanaActual = list.get(i) + list.get(i + 1) + list.get(i + 2)
+                if (ventanaActual > valorVentanaAnterior) {
+                    contador++
+                }
+                valorVentanaAnterior = ventanaActual
+                i++
+            }
+            return contador
+        }
+    }
+}
---
+++ src/test/kotlin/com/asegnz/adventofcode2021/Day1Test.kt
@@ -0,0 +1,24 @@
+package com.asegnz.adventofcode2021
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class Day1Test {
+    @Test
+    fun example() {
+        val incrementos = Day1.calculaMayores("src/main/resources/day1/example.txt");
+        Assertions.assertEquals(7, incrementos)
+    }
+
+    @Test
+    fun calculaIncrementos() {
+        val incrementos = Day1.calculaMayores("src/main/resources/day1/exam.txt");
+        println(incrementos)
+    }
+
+    @Test
+    fun ventanas() {
+        val incrementos = Day1.calculaVentana3("src/main/resources/day1/exam.txt")
+        println("Los incrementos son $incrementos")
+    }
+}
